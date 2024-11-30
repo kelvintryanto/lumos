443d2d8ecc9b58a3114ff3c7fb4b35e9127172ba
kelvintryanto <kelvin.tryanto@gmail.com>
2024-11-30 16:26:39 +0700

    fix:base_url to lumos back end

--- client/src/routers/index.jsx
+++ client/src/routers/index.jsx
@@ -10,7 +10,8 @@ import UpdateProfile from "../views/UpdateProfile";
 import ChangePassword from "../views/ChangePassword";
 import DeleteAccount from "../views/DeleteAccount";
 
-const base_url = "https://13.213.34.152";
+// const base_url = "https://13.213.34.152";
+const base_url = "lumos.kelvintryanto.xyz";
 
 export const router = createBrowserRouter([
   {
